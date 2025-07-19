98bffae61d7df299bb9596521b35bd426a24170c
QRZqiuye <flyboyok@naver.com>
2025-07-19 11:22:01 +0900

    C++기초: inline 함수

---
+++ 3-10.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+inline void SwapValues(int& a, int& b) {
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+int main() {
+	int a, b;
+	cout << "두 수를 입력하시오: ";
+	cin >> a >> b;
+	if (a < b) {
+		SwapValues(a, b);
+	}
+	cout << "큰수 = " << a << " 작은수 = " << b << endl;
+	return 0;
+}
