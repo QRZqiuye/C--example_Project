5606ee63f773a5e5f55de3d39d6edfa526a7c0db
QRZqiuye <flyboyok@naver.com>
2025-07-31 12:54:38 +0900

    C++기초: 대입 및 이동 대입 연산자 다중정의

--- VecF.h
+++ VecF.h
@@ -39,6 +39,23 @@ public:
 		}
 		cout << "]";
 	}
+
+	VecF & operator=(const VecF& fv) {
+		if (n != fv.n) {
+			delete[] arr;
+			arr = new float[n = fv.n];
+		}
+		memcpy(arr, fv.arr, sizeof(float) * n);
+		return *this;
+	}
+
+	VecF& operator=(VecF&& fv) {
+		delete[] arr;
+		n = fv.n;
+		arr = fv.arr;
+		fv.arr = nullptr;
+		return *this;
+	}
 };
 
 #endif
