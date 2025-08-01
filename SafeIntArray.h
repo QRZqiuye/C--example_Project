f07e09d3033201f5f5c1540146360d2b0b30e7f3
QRZqiuye <flyboyok@naver.com>
2025-08-01 11:59:23 +0900

    C++기초: [ ] 연산자 다중정의

---
+++ SafeIntArray.h
@@ -0,0 +1,34 @@
+#ifndef SAFEINTARRAY_H_INCLUDED
+#define SAFEINTARRAY_H_INCLUDED
+#include <iostream>
+
+class SafeIntArray {
+	int limit;
+	int* arr;
+public:
+	SafeIntArray(int n) :limit(n) {
+		arr = new int[n];
+	}
+	~SafeIntArray() {
+		delete[] arr;
+	}
+	int size() const {
+		return limit;
+	}
+	int& operator[](int i) {
+		if(i < 0 || i >= limit) {
+			std::cout << "첨자가 범위를 벗어났습니다.";
+			exit(EXIT_FAILURE);
+		}
+		return arr[i];
+	}
+	int operator[](int i) const {
+		if(i < 0 || i >= limit) {
+			std::cout << "첨자가 범위를 벗어났습니다.";
+			exit(EXIT_FAILURE);
+		}
+		return arr[i];
+	}
+};
+
+#endif
