9382f70298f7025bf7d1a464ad5edac5d4b54855
QRZqiuye <flyboyok@naver.com>
2025-08-03 12:57:12 +0900

    C++기초 : 스트링 클래스 예제

---
+++ 5-27.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "MyString.h"
+using namespace std;
+
+int main() {
+	MyString str1("MyString class");
+	MyString str2("Object Oriented");
+	MyString str3;
+
+	cout << str1 << endl;
+	str3 = "Programming";
+	cout << str3 << "의 문자열 길이는 ";
+	cout << str3.length() << endl;
+	str1 = str2;
+	cout << str1 << endl;
+	str1 = str2 + str3;
+	cout << str1 << endl;
+	MyString str4(str3);
+	cout << str4 << endl;
+	str2 += "Programming";
+	cout << str2 << endl;
+	str2[6] = '-';
+	cout << str2 << endl;
+	return 0;
+}
