8970e50ab5eaea02c11a99a4c3a73932d73e0318
QRZqiuye <flyboyok@naver.com>
2025-08-10 22:29:03 +0900

    C++기초: 파생클래스와 포인터

---
+++ Person3.h
@@ -0,0 +1,18 @@
+#ifndef PERSON3_H_INCLUDED
+#define PERSON3_H_INCLUDED
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Person
+{
+private:
+	string name;
+public:
+	Person(const string& n) : name(n) {}
+	string getName()const { return name; }
+	void print() const { cout << name; }
+};
+
+#endif
+
---
+++ Student3.h
@@ -0,0 +1,28 @@
+#ifndef STUDENT3_H_INCLUDED
+#define STUDENT3_H_INCLUDED
+#include <iostream>
+#include <string>
+#include "Person3.h"
+
+class Student :public Person
+{
+private:
+	string school;
+
+public:
+	Student(const string& n, const string& s) :Person(n), school(s) {}
+
+	string getSchool() const
+	{
+		return school;
+	}
+
+	void print() const {
+		Person::print();
+		cout << " goes to " << school;
+	}
+};
+
+#endif
+
+
