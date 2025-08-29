52e6c28d86f4a3fa8251ad145508ec1f3a4ccbe2
QRZqiuye <flyboyok@naver.com>
2025-08-29 23:33:46 +0900

    C++기초: 입출력과 파일 입출력의 응용

---
+++ 9-1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char a, b, c;
+	int n;
+	float f;
+	char str[20];
+
+	cout << "문자들을 입력하세요: ";
+	cin >> a >> b >> c >> n >> f >> str;
+	cout << "char a: " << a << endl;
+	cout << "char b: " << b << endl;
+	cout << "char c: " << c << endl;
+	cout << "int n: " << n << endl;
+	cout << "float f: " << f << endl;
+	cout << "string str: " << str << endl;
+	return 0;
+}
